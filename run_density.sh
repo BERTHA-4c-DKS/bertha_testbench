68acc7d3ec46931e6c8306b6a829577406a4acb5
Loriano Storchi <loriano@storchi.org>
2023-03-29 15:24:17 +0200

    minor fix

--- run_density.sh
+++ run_density.sh
@@ -21,8 +21,8 @@ echo "Num. of Threads: " $OMP_NUM_THREADS
 ./bertha.serial 1> run_"$numth"_"$BERTHA_KRGRID"_"$BERTHA_KAGRID".out 2> run_"$numth"_"$BERTHA_KRGRID"_"$BERTHA_KAGRID".err
 grep "total fitted density" run_"$numth"_"$BERTHA_KRGRID"_"$BERTHA_KAGRID".out  | tail -n 1
 grep "functional energy" run_"$numth"_"$BERTHA_KRGRID"_"$BERTHA_KAGRID".out  | tail -n 1
-grep "total analytical fitted density" run_"$numth"_"$BERTHA_KRGRID"_"$BERTHA_KAGRID".out  | tail -n 
-grep "convergence obtained in" run_"$numth"_"$BERTHA_KRGRID"_"$BERTHA_KAGRID".out .out  | tail -n 
+grep "total analytical fitted density" run_"$numth"_"$BERTHA_KRGRID"_"$BERTHA_KAGRID".out  | tail -n 1 
+grep "convergence obtained in" run_"$numth"_"$BERTHA_KRGRID"_"$BERTHA_KAGRID".out .out  | tail -n 1
 grep "total energy =" run_"$numth"_"$BERTHA_KRGRID"_"$BERTHA_KAGRID".out  | tail -n 1
 
 rm -f fittcoeff.restart ovap.out fockmtx.txt vct.out 
